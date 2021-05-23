587301c37c7941b5774ed9321ced752d2e9b8316
IsaackEz <isaackez@outlook.com>
2021-05-23 04:06:55 -0500

    Conection to Azure MySQL

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 module.exports = {
 	database: {
-		connectionLimit: 500,
 		host: process.env.DATABASE_HOST || 'localhost',
 		user: process.env.DATABASE_USER || 'root',
 		password: process.env.DATABASE_PASSWORD || '123',
 		database: process.env.DATABASE_NAME || 'dbMD',
+		ssl: true,
 	},
-	port: process.env.PORT || 8090,
+	port: process.env.PORT || 8080,
 };
