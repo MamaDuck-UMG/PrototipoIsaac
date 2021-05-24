24a0f98d8daa67d427c3a35c8bf53ba92e60ba95
IsaackEz <isaackez@outlook.com>
2021-05-23 19:17:54 -0500

    Write to json with db

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,31 @@
 const userCtrl = {};
 const pool = require('../database');
+const fs = require('fs');
 
 userCtrl.renderUserProfile = async (req, res, next) => {
 	const history = await pool.query('SELECT * FROM history');
 	res.render('profile', { history });
+	const coords = await pool.query('SELECT lati, lngi FROM history');
+	var features = [];
+	for (let i = 0; i < coords.length; i++) {
+		if (coords[i].lati != '') {
+			var feature = {
+				type: 'Feature',
+				geometry: {
+					type: 'Point',
+					coordinates: [coords[i].lngi, coords[i].lati],
+				},
+				properties: {
+					prop0: 'value0',
+				},
+			};
+			features.push(feature);
+		}
+	}
+	fs.writeFileSync(
+		'C:/GitHub/PrototipoIsaac/src/public/json/geojson.json',
+		JSON.stringify(features, null, 3)
+	);
 };
 
 module.exports = userCtrl;
