1c94fb153f370b5dc2155ffebbb0662bc98f1fd5
IsaackEz <isaackez@outlook.com>
2021-05-23 19:17:27 -0500

    Added coords to database and realtime

--- src/public/js/leaflet.js
+++ src/public/js/leaflet.js
@@ -1,72 +1,34 @@
-const map = L.map('map-template');
+var map = L.map('map-template');
+var map2 = L.map('map2');
+var map3 = L.map('map3');
+
 const tileURL = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
 const socket = io();
 L.tileLayer(tileURL).addTo(map);
 map.locate({ enableHighAccuracy: true });
 map.on('locationfound', (e) => {
 	coords = [e.latlng.lat, e.latlng.lng];
-	const marker = L.marker(coords);
-	marker.bindPopup('You are here!');
-	map.addLayer(marker);
-	map.setView([e.latlng.lat, e.latlng.lng], 13);
+	L.marker(coords).bindPopup('You are here!').addTo(map);
+	map.setView([e.latlng.lat, e.latlng.lng], 14);
 	socket.emit('userCoordinates', e.latlng);
 });
+
+L.tileLayer(tileURL).addTo(map2);
+map2.setView([20.6874157, -103.3578227], 11);
+
+L.tileLayer(tileURL).addTo(map3);
+map3.setView([20.6874157, -103.3578227], 11);
 
 const geoJson = '/json/geojson.json';
 
 fetch(geoJson)
 	.then((res) => res.json())
 	.then((data) => {
-		L.geoJSON(data).addTo(map);
+		L.geoJSON(data).addTo(map2);
 	});
 
 socket.on('newUserConnected', (coords) => {
 	const marker = L.marker([coords.lat, coords.lng]);
 	marker.bindPopup('New User!');
-	map.addLayer(marker);
+	map3.addLayer(marker);
 });
-
-longit = document.getElementsByTagName('p');
-latit = document.getElementsByTagName('h1');
-coordiLat = [];
-coordiLng = [];
-Array.from(latit).forEach((item, i) => (coordiLat[i] = item.innerHTML));
-Array.from(longit).forEach((item, i) => (coordiLng[i] = item.innerHTML));
-
-const feature = {
-	type: 'Feature',
-	geometry: {
-		type: 'Point',
-		coordinates: [parseFloat(coordiLng[0]), parseFloat(coordiLat[0])],
-	},
-	properties: {
-		prop0: 'value0',
-	},
-};
-console.log(feature);
-featureJ = JSON.stringify(feature);
-
-function geoFindMe() {
-	const lat = document.querySelector('#lat');
-	const lng = document.querySelector('#lng');
-
-	function success(position) {
-		const latitude = position.coords.latitude;
-		const longitude = position.coords.longitude;
-
-		lat.textContent = `${latitude}`;
-		lng.textContent = `${longitude}`;
-	}
-
-	function error() {
-		alert('Unable to retrieve your location');
-	}
-
-	if (!navigator.geolocation) {
-		alert('Geolocation is not supported by your browser');
-	} else {
-		navigator.geolocation.getCurrentPosition(success, error);
-	}
-}
-
-document.querySelector('#find-me').addEventListener('click', geoFindMe);
